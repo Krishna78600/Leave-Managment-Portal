0da11a0b7ed5f92bfa62ef7630f320776ffb4331
krishna123 <krishnatulaskar786.com>
2026-02-24 11:08:49 +0530

    Feat : setup maven project folder to create Backend

---
+++ backend/src/test/java/com/leavemgmt/leave_management/LeaveManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leavemgmt.leave_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
